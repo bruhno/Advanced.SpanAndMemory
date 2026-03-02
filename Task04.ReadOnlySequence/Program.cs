f7bbc414e3c964de0bec2588ca0981ca2842d6a3
gennadii_brukhno <gennadii.brukhno@altenar.com>
2026-03-02 21:43:40 +0300

    ArrayPool

---
+++ Task04.ReadOnlySequence/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Buffers;
+
+var s1 = new MySegment(new int[] { 1, 2, 3 });
+s1.SetNext(new(new int[] { 4, 5, 6 }));
+
+var sequence = new ReadOnlySequence<int>(s1, 0, s1.Next!, s1.Next!.Memory.Length);
+
+sequence = sequence.Slice(0, 5);
+
+foreach (var segment in sequence)
+{
+    foreach (var item in segment.Span)
+    {
+        Console.WriteLine(item);
+    }
+    Console.WriteLine("---");
+}
+
+class MySegment: ReadOnlySequenceSegment<int>
+{
+    public MySegment(ReadOnlyMemory<int> memory)
+    {
+        Memory = memory;
+    }
+
+    public void SetNext(MySegment segment)
+    {
+        Next = segment;
+        segment.RunningIndex = RunningIndex + Memory.Length;
+    }
+}
---
+++ Task06.ArrayPooling/Program.cs
@@ -0,0 +1,41 @@
+﻿
+using Newtonsoft.Json;
+using System.Buffers;
+
+var data = "[1,2,3]";
+
+var arr = ReadData(data);
+
+foreach (var item in arr)
+{
+    Console.WriteLine(item);
+}
+
+static int[] ReadData(string data)
+{
+    var serializer = new JsonSerializer();
+
+    using var reader = new JsonTextReader(new StringReader(data));
+
+    reader.ArrayPool = new MyArrayPool();
+
+    var value  = serializer.Deserialize<int[]>(reader);
+
+    return value;
+}
+
+internal class MyArrayPool: IArrayPool<char>
+{
+    public char[] Rent(int minimumLength)
+    {
+        return ArrayPool<char>.Shared.Rent(minimumLength);
+    }
+
+    public void Return(char[]? array)
+    {
+        if (array is not null)
+        {
+            ArrayPool<char>.Shared.Return(array);
+        }
+    }
+}
